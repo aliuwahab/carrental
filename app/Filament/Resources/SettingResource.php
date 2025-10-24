54876ece761527d6a6061d0b9620d6b89b498b5c
Aliu <aliuwahab@gmail.com>
2025-10-24 18:48:14 +0200

    implement full vehicle rental

--- app/Filament/Resources/SettingResource.php
+++ app/Filament/Resources/SettingResource.php
@@ -143,7 +143,6 @@ class SettingResource extends Resource
     {
         return [
             'index' => Pages\ListSettings::route('/'),
-            'create' => Pages\CreateSetting::route('/create'),
             'edit' => Pages\EditSetting::route('/{record}/edit'),
         ];
     }
